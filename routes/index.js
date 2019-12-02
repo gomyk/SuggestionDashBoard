998458599ad21fe79d1c9a3d0c10a60c780afd52
moonwon kim <gomyk@naver.com>
2019-12-02 09:21:47 +0900

    more error log

--- routes/index.js
+++ routes/index.js
@@ -17,21 +17,29 @@ router.post('/', function(req, res, next) {
   var parsed_json = JSON.parse(data);
 
   if(parsed_json == null){
-    res.send(500,'json parse error');
+    res.send(500,'Parse data : Json parse error');
   } else{
-    res.send(200,'OK');
+    if(parsed_json.bixby_client_version == undefined
+      || parsed_json.bixby_client_version == null) {
+      res.send(500,'Parse data : Cannot find client version');
+    }
+    else if(parsed_json.bixby_client_version <= '2.2.46.85') {
+      res.send(500,'Parse data : Low version');
+    } else {
+      res.send(200,'Parse data : OK...try send log');
+      request({
+        url: 'http://localhost:3003/suggestion',
+        method : 'POST',
+        json : parsed_json
+      },function (err,res,body) {
+          if(err){
+            console.log("Send log : Cannot send to logstash");
+          } else{
+            console.log("Send log : OK");
+          }
+      });
+    }
   }
-  request({
-    url: 'http://localhost:3003/suggestion',
-    method : 'POST',
-    json : parsed_json
-  },function (err,res,body) {
-      if(err){
-        console.log("Send log : Cannot send to logstash");
-      } else{
-        console.log("Send log : OK");
-      }
-  });
 });
 
 module.exports = router;
