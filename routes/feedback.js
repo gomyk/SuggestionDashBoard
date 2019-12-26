093f05513d88cc0f2e5311c1303442c7e8f8b816
moonwon kim <gomyk@naver.com>
2019-12-26 09:38:37 +0900

    Add feedback router

---
+++ routes/feedback.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var Feedback = require('../models/feedback.js');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if(req.query.q == undefined || req.query.q == null) {
+    console.log('query not found error');
+    res.send(500, 'query not found');
+    return;
+  }
+  var parsed_query = JSON.parse(req.query.q);
+  if(parsed_query == null) {
+    console.log('parsed query is null');
+    res.send(500, 'query parse error');
+    return;
+  }
+  Feedback.find(parsed_query, function(err , docs) {
+    if(err){
+      console.log(err);
+      res.send(500, err);
+      return;
+    }
+    console.log('OK');
+    res.json(docs);
+  });
+});
+
+module.exports = router;
