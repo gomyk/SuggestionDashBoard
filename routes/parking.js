58b48eed00b1fde46f510ce9cd6b8fe6e925a13e
moonwon kim <gomyk@naver.com>
2020-01-09 11:48:36 +0900

    Parking update

--- routes/parking.js
+++ routes/parking.js
@@ -100,40 +100,31 @@ router.get('/update', function(req, res, next) {
   var parsed_query = JSON.parse(req.query.q);
   var selection = null;
   var option = null;
+  var update = null;
   if(req.query.s != undefined) {
     selection = req.query.s;
   }
   if(req.query.o != undefined) {
     option = JSON.parse(req.query.o);
   }
+  if(req.query.u != undefined) {
+    update = JSON.parse(req.query.u);
+  }
   if(parsed_query == null) {
     console.log('parsed query is null');
     res.send(500, 'query parse error');
     return;
   }
-  Parking.find(parsed_query, selection, option, function(err , docs) {
+  Parking.update(parsed_query, update,function(err,result) {
+    console.log("update_with_query : ");
+    console.log(update);
     if(err){
-      console.log(err);
-      res.send(500, err);
-      return;
+      console.log("err");
+      res.send(500,'not ok');
+    } else {
+      console.log("OK_updated");
+      res.send(200,'ok');
     }
-    console.log('OK');
-    docs.forEach(doc => {
-      var time = doc.session_id.split('_')[0].split(' ');
-      var YMD = time[0].split('-');
-      var HMS = time[1].split(':');
-      var milli = HMS[2].split('.');
-
-      var date = new Date(YMD[0], YMD[1] - 1, YMD[2], HMS[0], HMS[1], milli[0]);
-
-      Feedback.update({_id:doc._id},{timestamp:date.getTime()+milli[1]},function(err,result) {
-        if(err){
-          console.log("err");
-        } else {
-          console.log("OK_updated");
-        }
-      });
-    });
   });
 });
 
